e1897b5432a229e1291f9f3b04ef3de0705cf2c1
Ashley Williams <ashley666ashley@gmail.com>
2016-11-02 15:48:54 -0400

    implement

---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const argv = require('yargs').argv;
+const request = require('request');
+const marky = require('marky-markdown');
+const cheerio = require('cheerio');
+
+require('colors')
+const jsdiff = require('diff');
+
+var pkg = argv.package;
+var get_readmes = function(pkg, callback) {
+  request('http://registry.npmjs.org/' + pkg, function (err, res, body) {
+    var pkg_details = JSON.parse(body);
+    get_npm(pkg_details);
+    get_github(pkg_details);
+    callback();
+  });
+};
+
+var get_npm = function(pkg_details) {
+  var npm_markup = marky(pkg_details.readme);
+  fs.writeFile('./npm_markup.html', npm_markup, 'utf-8');
+};
+
+var get_github = function(pkg_details) {
+  var latest = pkg_details["dist-tags"].latest;
+  var repo = pkg_details.versions[latest].repository;
+  if (is_github_repo(repo)) {
+    var url = make_github_url(repo.url);
+    request(url, function(err, res, body) {
+      var $ = cheerio.load(body);
+      fs.writeFile('./github_markup.html', $('article.markdown-body').html(), 'utf-8');
+    });
+  }
+};
+
+var is_github_repo = function(repo) {
+  return repo.type === 'git' && repo.url.match('github.com');
+}
+
+var make_github_url = function(repo_url) {
+  return remove_dot_git(repo_url).replace(/.*?:\/\//g, "https://");
+}
+
+var remove_dot_git = function(repo_url) {
+  if (repo_url.slice(-4) === '.git') {
+    return repo_url.slice(0, -4);
+  }
+};
+
+var diff_readmes = function() {
+  var npm = fs.readFileSync('./npm_markup.html', 'utf-8');
+  var github = fs.readFileSync('./github_markup.html', 'utf-8');
+
+  var diff = jsdiff.diffChars(npm, github);
+ 
+  diff.forEach(function(part){
+    // green for additions, red for deletions 
+    // grey for common parts 
+    var color = part.added ? 'green' :
+      part.removed ? 'red' : 'grey';
+    process.stderr.write(part.value[color]);
+  });
+ 
+  console.log();
+};
+
+get_readmes(pkg, diff_readmes);
